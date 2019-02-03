a8ecba137ee056b306b6c8ad4b81c1e64114a22a
Stefan Stoichev <stefan.stoichev@gmail.com>
2019-02-03 07:22:52 +0000

    Tests update - errors when cant connect ir doc is missing

--- tests/main.test.js
+++ tests/main.test.js
@@ -2,9 +2,31 @@ const enigma = require('enigma.js');
 const WebSocket = require('ws');
 const schema = require('enigma.js/schemas/12.20.0.json');
 const docMixin = require('../src/main.js');
+const OS = require('os')
 
-let qSession; 
+const OSUser = OS.userInfo().username;
+const testDoc = `C:\\Users\\${OSUser}\\Documents\\Qlik\\Sense\\Apps\\Helpdesk Management1.qvf`
+
+let qSession;
 let qGlobal;
+
+beforeAll(async function () {
+  try {
+    let qlikConnect = await connect()
+
+    try {
+      let qDoc = await qlikConnect.global.openDoc(testDoc)
+    } catch (e) {
+      console.log(`Test qvf do not exists`)
+      process.exit(1)
+    } finally {
+      await qlikConnect.session.close()
+    }
+  } catch (e) {
+    console.log("Unable to connecto to Qlik Sense")
+    process.exit(1)
+  }
+});
 
 beforeEach(async function () {
   let qlikConnect = await connect()
@@ -46,14 +68,14 @@ describe('initialization', async function () {
 
 describe('Selectioins', async function () {
   it('Selecting value in field to be ok', async function () {
-    let qDoc = await qGlobal.openDoc('C:\\Users\\Lenovo-Yoga-260\\Documents\\Qlik\\Sense\\Apps\\Helpdesk Management.qvf')
+    let qDoc = await qGlobal.openDoc(testDoc)
     let a1 = await qDoc.mixin.qSelections.selectInField({ fieldName: 'Case Owner Group', values: ['Operatioins'] })
 
     expect(a1).toBeTruthy
   });
 
   it('Check selected values are correct', async function () {
-    let qDoc = await qGlobal.openDoc('C:\\Users\\Lenovo-Yoga-260\\Documents\\Qlik\\Sense\\Apps\\Helpdesk Management.qvf')
+    let qDoc = await qGlobal.openDoc(testDoc)
     let a1 = await qDoc.mixin.qSelections.selectInField({ fieldName: 'Case Owner Group', values: ['Operations'] })
     let a2 = await qDoc.mixin.qSelections.getCurrSelectionFields()
 
